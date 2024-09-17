d4f5acaac9ea989cbda11176f4542e77e78fc843
MarianoMaldonado-dev <cyberwargamesproductions@gmail.com>
2024-09-17 16:15:55 -0300

    frontend/feature/crud/'refork landing page and add all whatsapp links. I declare this version as Release v1.0 The responsive is functional'

--- landing-social/scripts/update_year.js
+++ landing-social/scripts/update_year.js
@@ -4,5 +4,9 @@ const year = new Date().getFullYear(); //Se obtiene el año en curso y se almace
 document.getElementById('current-year').textContent= year;
 //console.log(year);
 
-/*    { DEV</>CODE } Informatic Solutions
+/*    
+    Copyright:
+        { DEV</>CODE } Informatic Solutions
+         https://github.com/MarianoMaldonado-dev/
+    Licencia: GNU General Public License v3.0
 */
